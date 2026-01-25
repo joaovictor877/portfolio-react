3ab0527977ce40651d46ae7cc036d7cf1e6bbd78
Joao <silvasouzajoaovictor877@gmail.com>
2026-01-25 14:29:40 -0300

    push env

--- api/db.js
+++ api/db.js
@@ -1,7 +1,10 @@
 import mysql from 'mysql2/promise';
 
+const [host, port] = (process.env.MYSQL_HOST || '127.0.0.1').split(':');
+
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST || '192.168.15.28',
+  host: host,
+  port: port ? parseInt(port) : 3306,
   user: process.env.MYSQL_USER || 'portfolio',
   password: process.env.MYSQL_PASSWORD || '04B43ab2a',
   database: process.env.MYSQL_DATABASE || 'portfolio',
